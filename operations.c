9742a97b48546caf3a2035f84faa141d13bdc8bb
Diogo Fernandes <diogo.a.f.fernandes@gmail.com>
2017-03-11 19:14:43 +0000

    digas

--- operations.c
+++ operations.c
@@ -1,16 +1,20 @@
+#define __poli_h__
 #include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "poli.h"
 
-Polinomio *normalize(LinkedList *l); 	
-Polinomio *derivate(LinkedList *l);
+Polinomio *Norm(LinkedList *l); //Normalização de polinómios
+Polinomio *Deriv(LinkedList *l); //Derivaçao de polinomios
+Polinomio *Integra(LinkedList *l); //Integraçao de polinomios
 
-
-Polinomio *normalize(LinkedList *l){
+Polinomio *Norm(LinkedList *l){
 
 	LinkedList *aux = NULL;
+
 	LinkedList *p = l;
 
-	for(; l!=NULL; l=l-> next){
+	for(; l!=NULL ; l=l-> next){
 		for(p=l->next ; p!=NULL ; p=p->next){
 			if(l->pols.pol.var == p->pols.pol.var && l->pols.pol.exp == p->pols.pol.exp && p->pols.pol.visited == false){
 				aux->pols.pol.exp = l->pols.pol.exp;
@@ -24,14 +28,35 @@ Polinomio *normalize(LinkedList *l){
 }
 
 
-Polinomio *derivate(LinkedList *l){
+Polinomio *Deriv(LinkedList *l){
+	LinkedList *aux = NULL;
+
 	for(; l!= NULL; l=l->next){
 		if(l->kind = CONS)
 			aux->pols.pol.coef = 0;
+
 		else{
 			aux->pols.pol.coef = l->pols.pol.coef * l->pols.pol.exp;
 			aux->pols.pol.exp = l->pols.pol.exp - 1;
 		}
 	}
+	return aux;
+}
+
+Polinomio *Integra(LinkedList *l){
+	LinkedList *aux = NULL;
+
+	for(;l!=NULL;l=l->next){
+		if(l->kind = CONS){
+			aux->pols.pol.coef = l->pols.cons;
+			aux->pols.pol.var = 'x';
+			aux->pols.pol.exp = 1;
+		}
+		else{
+			aux->pols.pol.coef = l->pols.pol.coef / (l->pols.pol.exp +1);
+			aux->pols.pol.exp = l->pols.pol.exp;
+		}
+	}
+
 	return aux;
 }
