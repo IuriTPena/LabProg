9ab09952fbf092c333a045aa818f55e450fe90b0
iuritpena <iuritavarespena@gmail.com>
2017-03-11 23:08:29 +0000

    devided scan into functions

--- man.c
+++ man.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
 #include "poli.h"
 
+LinkedList *scanPolinomio(LinkedList *l);
+LinkedList *scanConstante(LinkedList *l);
+
 int main (int argc, char **argv) {
 
     LinkedList *all = NULL;
+    
+    int flag = 0;
 
-    printf(" ## Inserir polinómios\n");
+    printf("Inserir polinómio?\n0. Não\n1. Sim\n");
+    scanf("%d", &flag);
+    if(flag) all = scanPolinomio(all);
+
+    printf("Inserir constante?\n0. Não\n1. Sim\n");
+    scanf("%d", &flag);
+    if(flag) all = scanConstante(all);
+
+
+    return 0;
+}
+
+LinkedList *scanPolinomio(LinkedList *l) {
     int more = 0;
     do {
         float coe = 0.0;
@@ -18,27 +35,27 @@ int main (int argc, char **argv) {
         printf("Expoente: ");
         scanf("%d", &exp);
         
-        all = makeList(makePoly(coe, var, exp), NULL);
-        all = all->next;
+        l = makeList(makePoly(coe, var, exp), NULL);
+        l = l->next;
 
         printf("Há mais?\n0. Não\n1. Sim\n");
         scanf("%d", &more);
-    } while(more);
-    
-    printf(" ## Inserir constantes\n");
+    } while(more);    
+    return l;
+}
+
+LinkedList *scanConstante(LinkedList *l) {
+    int more = 0;
     do {
         int i = 0;
         printf("Constante: ");
         scanf("%d", &i);
 
-        all = makeList(makeCons(i), NULL);
-        all = all->next;
+        l = makeList(makeCons(i), NULL);
+        l = l->next;
 
         printf("Há mais?\n0. Não\n1. Sim\n");
         scanf("%d", &more);        
     } while(more);
-    
-
-
-    return 0;
+    return l;   
 }
