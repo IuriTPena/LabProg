c59376e9a115e2bc0ad4b4e36ad651481a409af7
iuritpena <iuritavarespena@gmail.com>
2017-03-12 01:15:38 +0000

    declarations

---
+++ operations.h
@@ -0,0 +1,11 @@
+#ifndef __operations_h__
+#define __operations_h__
+
+#include "poli.h"
+
+LinkedList *normalizar(LinkedList *l);
+LinkedList *somar(LinkedList *l);
+LinkedList *derivar(LinkedList *l);
+LinkedList *integrar(LinkedList *l);
+
+#endif
