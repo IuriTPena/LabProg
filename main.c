c0e916957566b6667596b1495419cced6526eab8
IuriTPena <iuritavarespena@gmail.com>
2017-03-16 10:48:20 +0000

    bugfix: redefined scan

--- main.c
+++ main.c
@@ -2,28 +2,20 @@
 #include "operations.h"
 
 int printPolinomio(LinkedList *l);
-LinkedList *scanMonomio(LinkedList *l);
-LinkedList *scanConstante(LinkedList *l);
+LinkedList *scanPolinomio();
+LinkedList *scanConstante();
+LinkedList *scanMonomio();
 
 int main (int argc, char **argv) {
 
-    LinkedList *all = NULL;
-    LinkedList *head = all;
-    
-    int flag = 0;
-    printf("Inserir mononómio?\n0. Não\n1. Sim\n");
-    scanf("%d", &flag);
-    if(flag) all = scanMonomio(all);
-
-    printf("Inserir constante?\n0. Não\n1. Sim\n");
-    scanf("%d", &flag);
-    if(flag) all = scanConstante(all);
+    LinkedList *p = NULL;
+    p = scanPolinomio();
 
     int oper;
     printf("Escolha as operações\n0. Imprimir\n1. Normalizar\n2. Somar\n3. Derivar\n4. Integrar\n");
     scanf("%d", &oper);
     switch (oper) {
-        case 0: printPolinomio(head); break;
+        case 0: printPolinomio(p); break;
       /*  case 1: normalizar(head); break;
         case 2: somar(head); break;
         case 3: derivar(head); break;
@@ -48,42 +40,53 @@ int printPolinomio(LinkedList *l) {
     return 0;
 }
 
-LinkedList *scanMonomio() {
+LinkedList *scanPolinomio() {
 	LinkedList *l = NULL;
-    int more = 0;
-    do {
-        float coe = 0.0;
-        char var[VARSIZE];
-        int exp = 0;
-        printf("Coeficiente: ");
-        scanf("%f", &coe);
-        printf("Variável: ");
-        scanf("%s", var);
-        printf("Expoente: ");
-        scanf("%d", &exp);
-        
-        l = makeList(makeMono(coe, var, exp), l);
-        //l = l->next;
+    
+    int nMono = 0;
+    printf("Nº de monómios a inserir: ");  
+    scanf("%d", &nMono);
 
-        printf("Há mais?\n0. Não\n1. Sim\n");
-        scanf("%d", &more);
-    } while(more);    
+    while(nMono-- > 0)
+        l = scanMonomio();
+
+
+    int nCons = 0;
+    printf("Nº de constantes a inserir: ");    
+    scanf("%d", &nCons);
+
+    while(nCons-- > 0)
+        l = scanConstante();
+
+    return l;
+}
+
+LinkedList *scanMonomio() {
+    LinkedList *l = NULL;
+
+    float coe = 0.0;
+    char var[VARSIZE];
+    int exp = 0;
+    printf("Coeficiente: ");
+    scanf("%f", &coe);
+    printf("Variável: ");
+    scanf("%s", var);
+    printf("Expoente: ");
+    scanf("%d", &exp);
+        
+    l = makeList(makeMono(coe, var, exp), l);
+
     return l;
 }
 
 LinkedList *scanConstante() {
     LinkedList *l = NULL;
-    int more = 0;
-    do {
-        int i = 0;
-        printf("Constante: ");
-        scanf("%d", &i);
 
-        l = makeList(makeCons(i), l);
-        //l = l->next;
+    int i = 0;
+    printf("Constante: ");
+    scanf("%d", &i);
 
-        printf("Há mais?\n0. Não\n1. Sim\n");
-        scanf("%d", &more);        
-    } while(more);
+    l = makeList(makeCons(i), l);
+
     return l;   
 }
