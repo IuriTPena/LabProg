b4e445aa9f389c7a4ee0766fbe7213cfdd20bda9
iuritpena <iuritavarespena@gmail.com>
2017-03-17 22:36:35 +0000

    added delete declaration

--- operations.h
+++ operations.h
@@ -4,6 +4,7 @@
 #include "poli.h"
 
 int printPolinomio(LinkedList *l);
+LinkedList *delete(LinkedList *head, LinkedList *del);
 LinkedList *normalizar(LinkedList *l);
 LinkedList *somar(LinkedList *n1, LinkedList *n2);
 LinkedList *derivar(LinkedList *l);
